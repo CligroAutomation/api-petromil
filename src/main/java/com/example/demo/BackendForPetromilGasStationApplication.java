2f2ae16be85cdc7022bdd41b3772b5fb3aced04e
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2025-01-11 14:18:13 -0500

    Refactor main application to use ApplicationContext and display encoded password

--- src/main/java/com/example/demo/BackendForPetromilGasStationApplication.java
+++ src/main/java/com/example/demo/BackendForPetromilGasStationApplication.java
@@ -1,22 +1,20 @@
 package com.example.demo;
 
-import jakarta.annotation.PostConstruct;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class BackendForPetromilGasStationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendForPetromilGasStationApplication.class, args);
+		ApplicationContext context = SpringApplication.run(BackendForPetromilGasStationApplication.class, args);
 
 		System.out.println("Zona horaria actual (TimeZone): " + TimeZone.getDefault().getID());
 		System.out.println("Zona horaria actual (ZoneId): " + ZoneId.systemDefault().getId());
@@ -24,9 +22,7 @@ public class BackendForPetromilGasStationApplication {
 		ZonedDateTime now = ZonedDateTime.now();
 		System.out.println("Hora actual: " + now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z")));
 
-
+		PasswordEncoder passwordEncoder = context.getBean(PasswordEncoder.class);
+		System.out.println(passwordEncoder.encode("cligro1234"));
 	}
-
-
-
 }
